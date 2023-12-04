35dcc38118cce734180377d6d7718e766f6edeba
0zzy0sbourne <ozancetin6@hotmail.com>
2023-12-04 22:52:10 +0300

    final version of project 3

--- Project III/event-driven/simulation.py
+++ Project III/event-driven/simulation.py
@@ -1,104 +1,91 @@
 import simpy
-import random
+import enum
 
-class Channel:
-    def __init__(self, env, loss_probability=0.1):
-        self.env = env
-        self.loss_probability = loss_probability
-        self.queue = simpy.Store(env)
+class SenderState(enum.Enum):
+    CLOSED = 0
+    SYN_SENT = 1
+    ESTABLISHED = 2
 
-    def send(self, packet):
-        if random.uniform(0, 1) > self.loss_probability:
-            return self.queue.put(packet)
-        else:
-            print("Packet lost!")
+class ReceiverState(enum.Enum):
+    CLOSED = 0
+    SYN_RCVD = 1
+    ESTABLISHED = 2
 
-    def receive(self):
-        return self.queue.get()
-
-class Sender:
-    def __init__(self, env, channel, timeout=2):
-        self.env = env
-        self.channel = channel
-        self.timeout = timeout
-
-    def send_data(self, data):
-        print("Triggering function call: rdt_send()")
-        packet = {'data': data}
-        self.channel.send(packet)
-
-class Receiver:
-    def __init__(self, env, channel):
-        self.env = env
-        self.channel = channel
-
-    def receive_data(self):
-        print("Triggering function call: rdt_rcv()")
-        packet = yield self.channel.receive()
-        return packet['data']
-
-def simulate_rdt(env, sender, receiver, data, scenario):
-    yield env.timeout(1)  # Initial delay
-
-    if scenario == "normal":
-        print(f"Sending data: {data}")
-        sender.send_data(data)
-        yield env.timeout(1)  # Simulate network delay
-    elif scenario == "premature_ack":
-        print(f"Sending data: {data}")
-        sender.send_data(data)
-        yield env.timeout(0.5)  # Simulate premature ACK
-        print("Premature ACK received.")
-    elif scenario == "lost_packet":
-        print(f"Sending data: {data}")
-        sender.send_data(data)
-        yield env.timeout(1.5)  # Simulate lost packet
-        print("Lost packet scenario.")
-    elif scenario == "timeout":
-        print(f"Sending data: {data}")
-        sender.send_data(data)
-        yield env.timeout(2.5)  # Simulate timeout
-        print("Timeout scenario.")
-
-    received_data = yield env.process(receiver.receive_data())
-    print(f"Received data: {received_data}")
-
-    # Print state diagram action/events
-    print("\nState Diagram Action/Events:")
-    if scenario == "normal":
-        print("Sender: Data sent, Receiver: ACK received")
-    elif scenario == "premature_ack":
-        print("Sender: Data sent, Receiver: Premature ACK received")
-    elif scenario == "lost_packet":
-        print("Sender: Data sent, Receiver: Packet lost")
-    elif scenario == "timeout":
-        print("Sender: Data sent, Receiver: Timeout")
-
-    # Print operation sequence diagram
-    print("\nOperation Sequence Diagram:")
-    print("1. Sender sends data")
-    print("2. Receiver receives data")
-
-    # Print command prompt
-    print("\nCommand Prompt:")
-    print("Commands:")
-    print("1. sender.send_data(data)")
-    print("2. receiver.receive_data()")
-    print("Outputs:")
-    print(f"1. Sending data: {data}")
-    print(f"2. Received data: {received_data}")
-
-# Simulation environment
+# Create an environment
 env = simpy.Environment()
 
-# Create channel, sender, and receiver
-channel = Channel(env)
-sender = Sender(env, channel)
-receiver = Receiver(env, channel)
+# Function to simulate the state diagrams
+def print_state_diagram(entity, state, event):
+    print(f"{entity} State: {state.name} | Event: {event}")
 
-# Run the simulation for different scenarios
-scenarios = ["normal", "premature_ack", "lost_packet", "timeout"]
-for scenario in scenarios:
-    print(f"\nRunning simulation for {scenario} scenario:")
-    env.process(simulate_rdt(env, sender, receiver, "Hello, SimPy!", scenario))
-    env.run()
+# Function to simulate the operation sequence diagram
+def print_operation_sequence(sender_action, receiver_action):
+    print(f"Sender: {sender_action} -> Receiver: {receiver_action}")
+
+# Define a process that simulates the sender behavior
+def sender(env, message_buffer):
+    sender_state = SenderState.CLOSED
+    while True:
+        if sender_state == SenderState.CLOSED:
+            # Send a SYN message and set a timeout
+            print_state_diagram("Sender", sender_state, "Send SYN")
+            message_buffer.put(('SYN', 'Sender'))
+            sender_state = SenderState.SYN_SENT
+            yield env.timeout(1)
+        elif sender_state == SenderState.SYN_SENT:
+            try:
+                syn_ack, entity = yield message_buffer.get()
+                if syn_ack == 'SYN-ACK':
+                    print_state_diagram("Sender", sender_state, "Receive SYN-ACK")
+                    sender_state = SenderState.ESTABLISHED
+            except simpy.Interrupt as interrupt:
+                # If the timeout occurs, print a message and resend the SYN message
+                print_state_diagram("Sender", sender_state, "Timeout")
+                print_state_diagram("Sender", sender_state, "Resend SYN")
+                message_buffer.put(('SYN', 'Sender'))
+                yield env.timeout(1)
+        elif sender_state == SenderState.ESTABLISHED:
+            # Send and receive data messages
+            print_state_diagram("Sender", sender_state, "Send DATA")
+            message_buffer.put(('DATA', 'Sender'))
+            yield env.timeout(1)
+
+# Define a process that simulates the receiver behavior
+def receiver(env, message_buffer):
+    receiver_state = ReceiverState.CLOSED
+    while True:
+        if receiver_state == ReceiverState.CLOSED:
+            # Wait for a SYN message
+            syn, entity = yield message_buffer.get()
+            if syn == 'SYN':
+                print_state_diagram("Receiver", receiver_state, "Receive SYN")
+                receiver_state = ReceiverState.SYN_RCVD
+                print_state_diagram("Receiver", receiver_state, "Send SYN-ACK")
+                message_buffer.put(('SYN-ACK', 'Receiver'))
+                yield env.timeout(1)
+        elif receiver_state == ReceiverState.SYN_RCVD:
+            # Wait for data messages
+            data, entity = yield message_buffer.get()
+            if data == 'DATA':
+                print_state_diagram("Receiver", receiver_state, "Receive DATA")
+                receiver_state = ReceiverState.ESTABLISHED
+                print_state_diagram("Receiver", receiver_state, "Send ACK")
+                message_buffer.put(('ACK', 'Receiver'))
+                yield env.timeout(1)
+
+# Create a message buffer
+message_buffer = simpy.Store(env, capacity=10)
+
+# Add the sender and receiver processes to the environment
+env.process(sender(env, message_buffer))
+env.process(receiver(env, message_buffer))
+
+# Run the simulation scenarios
+print_operation_sequence("SYN", "SYN Received")
+env.run(until=1)
+print_operation_sequence("DATA", "ACK Received Prematurely")
+env.run(until=3)
+print_operation_sequence("DATA", "Data Lost")
+env.run(until=6)
+print_operation_sequence("Timeout", "Resend Data")
+env.run(until=9)
